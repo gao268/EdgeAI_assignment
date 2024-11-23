2f76f46dc0a0612472e8cbd88aaa16ece6b21c97
gao268 <1005092089@qq.com>
2024-11-23 18:25:13 +0800

    Add files via upload

---
+++ my-detection-assignment3.py
@@ -0,0 +1,11 @@
+import jetson.inference
+import jetson.utils
+net = jetson.inference.detectNet("ssd-mobilenet-v2", threshold=0.5)
+img=jetson.utils.loadImage("/home/nvidia/jetson-inference/examples/1.jpg")
+detections=net.Detect(img)
+print(detections[0])
+display = jetson.utils.glDisplay()
+while display.IsOpen():
+    display.RenderOnce(img)
+    display.SetTitle("Detecting objects...")
+    
